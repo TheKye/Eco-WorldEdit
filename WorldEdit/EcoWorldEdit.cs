afd795339831af1539cc51842ffd487c4377af5f
Tavren <tavren.dragon@gmail.com>
2022-11-22 19:47:54 +0300

    2.4.0 for Eco 0.9.7.1 Nuget

--- WorldEdit/EcoWorldEdit.cs
+++ WorldEdit/EcoWorldEdit.cs
@@ -5,7 +5,7 @@ namespace Eco.Mods.WorldEdit
 {
 	public class EcoWorldEdit : IModKitPlugin, IServerPlugin, IInitializablePlugin
 	{
-		public const string Version = "2.3.0";
+		public const string Version = "2.4.0";
 		public const string SchematicDirectoryName = "Blueprints";
 		public const string SchematicDefaultExtension = ".ecobp";
 
