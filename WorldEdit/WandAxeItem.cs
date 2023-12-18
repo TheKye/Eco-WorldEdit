f29d8f63a5eb62bf267faf500c50012c8969ea36
kye <kye-a@hotmail.com>
2023-12-18 17:27:03 +1100

    Fixed Wand For world edit

--- WorldEdit/WandAxeItem.cs
+++ WorldEdit/WandAxeItem.cs
@@ -9,42 +9,47 @@
 	using Eco.Gameplay.Interactions.Interactors;
 	using Eco.Gameplay.Items;
 	using Eco.Gameplay.Players;
-    using Eco.Shared.Items;
+	using Eco.Mods.TechTree;
+	using Eco.Shared.Items;
     using Eco.Shared.Localization;
     using Eco.Shared.Math;
+	using Eco.Shared.Networking;
 	using Eco.Shared.Serialization;
     using Eco.Shared.SharedTypes;
     using Eco.Shared.Utils;
+	using Eco.World.Blocks;
 
-    [Serialized]
+	[Serialized]
 	[LocDisplayName("Wand Tool")]
 	[LocDescription("Does magical World Edit things")]
 	[Category("Hidden")]
-	public class WandAxeItem : ToolItem, IInteractor
+	[CanAirInteraction]
+    public class WandAxeItem : HammerItem, IInteractor
     {
         public override float DurabilityRate { get { return 0; } }
         public override IDynamicValue SkilledRepairCost => skilledRepairCost;
 		private static IDynamicValue skilledRepairCost = new ConstantValue(1);
 		public override ItemCategory ItemCategory => ItemCategory.Devtool;
+		public override LocString GetNoSuitablePickupTargetFailureMessage(Inventory inventory) => Localizer.DoStr("");
 
-		public override bool IsValidForInteraction(Item item)
+        public override bool IsValidForInteraction(Item item)
 		{
 			BlockItem blockItem = item as BlockItem;
-			return blockItem is not null;
+			return blockItem.BlockTypes.Count() >= 1;
 		}
 
-        [Interaction(InteractionTrigger.LeftClick, overrideDescription: "Set First Position", tags: BlockTags.Block, animationDriven: false, interactionDistance: 15)]
+        [Interaction(InteractionTrigger.LeftClick, overrideDescription: "Set First Position", flags: InteractionFlags.BlocksOtherInteraction, authRequired: AccessType.None)]
         public void SetFirstPos(Player player, InteractionTriggerInfo trigger, InteractionTarget target)
 		{
-			Log.WriteWarningLineLocStr("WandAxeItem: SetFirstPos");
 			try
 			{
-				if (!target.IsBlock) { return; }
-				if (target.BlockPosition is null || !target.BlockPosition.HasValue) { return; }
+                if (!target.IsBlock) { return; }
+                var pos = target.BlockPosition.Value;
+                var block = World.World.GetBlock(pos);
 
-				Vector3i pos = target.BlockPosition.Value;
+                if (block == null) return;
 
-				pos.X = pos.X < 0 ? pos.X + Shared.Voxel.World.VoxelSize.X : pos.X;
+                pos.X = pos.X < 0 ? pos.X + Shared.Voxel.World.VoxelSize.X : pos.X;
 				pos.Z = pos.Z < 0 ? pos.Z + Shared.Voxel.World.VoxelSize.Z : pos.Z;
 
 				pos.X = pos.X % Shared.Voxel.World.VoxelSize.X;
@@ -61,16 +66,16 @@
 			}
 		}
 
-        [Interaction(InteractionTrigger.RightClick, overrideDescription: "Set Second Position", tags: BlockTags.Block, animationDriven: false, interactionDistance: 15)]
+        [Interaction(InteractionTrigger.RightClick, overrideDescription: "Set Second Position", flags: InteractionFlags.BlocksOtherInteraction, authRequired: AccessType.None)]
         public void SetSecondPos(Player player, InteractionTriggerInfo triggerInfo, InteractionTarget target)
 		{
-            Log.WriteWarningLineLocStr("WandAxeItem: SetSecondPos");
             try
 			{
                 if (!target.IsBlock) { return; }
-                if (target.BlockPosition is null || !target.BlockPosition.HasValue) { return; }
+                var pos = target.BlockPosition.Value;
+                var block = World.World.GetBlock(pos);
 
-                Vector3i pos = target.BlockPosition.Value;
+				if (block == null) return;
 
 				pos.X = pos.X < 0 ? pos.X + Shared.Voxel.World.VoxelSize.X : pos.X;
 				pos.Z = pos.Z < 0 ? pos.Z + Shared.Voxel.World.VoxelSize.Z : pos.Z;
