5e2c9a70cbf9c28c4f3af9cb7b67be8134179453
Tavren <tavren.dragon@gmail.com>
2023-12-20 11:44:41 +0300

    Wand fixed properly

--- WorldEdit/WandAxeItem.cs
+++ WorldEdit/WandAxeItem.cs
@@ -6,7 +6,6 @@
 	using Eco.Gameplay.Interactions.Interactors;
 	using Eco.Gameplay.Items;
 	using Eco.Gameplay.Players;
-	using Eco.Mods.TechTree;
 	using Eco.Shared.Items;
 	using Eco.Shared.Localization;
 	using Eco.Shared.Math;
@@ -18,28 +17,22 @@
 	[LocDisplayName("Wand Tool")]
 	[LocDescription("Does magical World Edit things")]
 	[Category("Hidden")]
-	public class WandAxeItem : HammerItem, IInteractor
-	{
-		public override float DurabilityRate { get { return 0; } }
-		public override IDynamicValue SkilledRepairCost => skilledRepairCost;
+	public class WandAxeItem : ToolItem, IInteractor
+    {
+        public override float DurabilityRate { get { return 0; } }
+        public override IDynamicValue SkilledRepairCost => skilledRepairCost;
 		private static IDynamicValue skilledRepairCost = new ConstantValue(1);
 		public override ItemCategory ItemCategory => ItemCategory.Devtool;
-		public override LocString GetNoSuitablePickupTargetFailureMessage(Inventory inventory) => Localizer.DoStr("");
 
-		public override bool IsValidForInteraction(Item item)
-		{
-			BlockItem blockItem = item as BlockItem;
-			return blockItem.BlockTypes.Count() >= 1;
-		}
-
-		[Interaction(InteractionTrigger.LeftClick, overrideDescription: "Set First Position", flags: InteractionFlags.BlocksOtherInteraction, authRequired: AccessType.None)]
-		public void SetFirstPos(Player player, InteractionTriggerInfo trigger, InteractionTarget target)
+        [Interaction(InteractionTrigger.LeftClick, overrideDescription: "Set First Position", animationDriven: false, interactionDistance: 15, authRequired: AccessType.None)]
+        public void SetFirstPos(Player player, InteractionTriggerInfo trigger, InteractionTarget target)
 		{
 			try
 			{
 				if (!target.IsBlock) { return; }
 				if (target.BlockPosition is null || !target.BlockPosition.HasValue) { return; }
-				var pos = target.BlockPosition.Value;
+
+				Vector3i pos = target.BlockPosition.Value;
 
 				pos.X = pos.X < 0 ? pos.X + Shared.Voxel.World.VoxelSize.X : pos.X;
 				pos.Z = pos.Z < 0 ? pos.Z + Shared.Voxel.World.VoxelSize.Z : pos.Z;
@@ -50,7 +43,7 @@
 				UserSession userSession = WorldEditManager.GetUserSession(player.User);
 				userSession.SetFirstPosition(pos);
 
-				player.MsgLoc($"First Position set to ({pos.x}, {pos.y}, {pos.z})");
+                player.MsgLoc($"First position set to ({pos.x}, {pos.y}, {pos.z})");
 			}
 			catch (Exception e)
 			{
@@ -58,14 +51,15 @@
 			}
 		}
 
-		[Interaction(InteractionTrigger.RightClick, overrideDescription: "Set Second Position", flags: InteractionFlags.BlocksOtherInteraction, authRequired: AccessType.None)]
-		public void SetSecondPos(Player player, InteractionTriggerInfo triggerInfo, InteractionTarget target)
+        [Interaction(InteractionTrigger.RightClick, overrideDescription: "Set Second Position", animationDriven: false, interactionDistance: 15, authRequired: AccessType.None)]
+        public void SetSecondPos(Player player, InteractionTriggerInfo triggerInfo, InteractionTarget target)
 		{
-			try
+            try
 			{
-				if (!target.IsBlock) { return; }
-				if (target.BlockPosition is null || !target.BlockPosition.HasValue) { return; }
-				var pos = target.BlockPosition.Value;
+                if (!target.IsBlock) { return; }
+                if (target.BlockPosition is null || !target.BlockPosition.HasValue) { return; }
+
+                Vector3i pos = target.BlockPosition.Value;
 
 				pos.X = pos.X < 0 ? pos.X + Shared.Voxel.World.VoxelSize.X : pos.X;
 				pos.Z = pos.Z < 0 ? pos.Z + Shared.Voxel.World.VoxelSize.Z : pos.Z;
@@ -76,8 +70,8 @@
 				UserSession userSession = WorldEditManager.GetUserSession(player.User);
 				userSession.SetSecondPosition(pos);
 
-				player.MsgLoc($"Second Position set to ({pos.x}, {pos.y}, {pos.z})");
-			}
+                player.MsgLoc($"Second position set to ({pos.x}, {pos.y}, {pos.z})");
+            }
 			catch (Exception e)
 			{
 				Log.WriteError(Localizer.Do($"{e}"));
