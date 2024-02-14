4f3a30de901015002c37a23340e3e43710d3f483
Tavren <tavren.dragon@gmail.com>
2024-02-14 10:36:20 +0300

    Update EcoWorldEdit.cs
    Print version on startup an version refactoring #81
    Taking version information from assembly, no more two places to change version number

--- WorldEdit/EcoWorldEdit.cs
+++ WorldEdit/EcoWorldEdit.cs
@@ -1,13 +1,18 @@
 ﻿using Eco.Core.Plugins.Interfaces;
 using Eco.Core.Utils;
+using Eco.Shared.Localization;
+using Eco.Shared.Utils;
+using System.Reflection;
 
 namespace Eco.Mods.WorldEdit
 {
 	public class EcoWorldEdit : IModKitPlugin, IServerPlugin, IInitializablePlugin
 	{
-		public const string Version = "2.6.4";
+		public static readonly string Version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
 		public const string SchematicDirectoryName = "Blueprints";
 		public const string SchematicDefaultExtension = ".ecobp";
+
+		public EcoWorldEdit() { Log.WriteLine(Localizer.Do($"Using WorldEdit version {Version}")); }
 
 		public void Initialize(TimedTask timer)
 		{
