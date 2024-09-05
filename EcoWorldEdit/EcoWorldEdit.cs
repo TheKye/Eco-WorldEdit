9cf36c38cc2b4c92ddc09d0b35fcfa9be1a15620
StalEF <driving-kovrov@yandex.ru>
2024-09-05 22:01:03 +0300

    add ModRegistration

--- EcoWorldEdit/EcoWorldEdit.cs
+++ EcoWorldEdit/EcoWorldEdit.cs
@@ -6,13 +6,20 @@ using System.Reflection;
 
 namespace Eco.Mods.WorldEdit
 {
-	public class EcoWorldEdit : IModKitPlugin, IServerPlugin, IInitializablePlugin
-	{
+	public class EcoWorldEdit : IModKitPlugin, IServerPlugin, IInitializablePlugin, IModInit
+    {
 		public static readonly string Version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
 		public const string SchematicDirectoryName = "Blueprints";
 		public const string SchematicDefaultExtension = ".ecobp";
 
 		public EcoWorldEdit() { Log.WriteLine(Localizer.Do($"Using WorldEdit version {Version}")); }
+		
+		public static ModRegistration Register() => new()
+        {
+            ModName = "EcoWorldEdit",
+            ModDescription = "WorldEdit is the ultimate map editor to get creative, do not engage in routine.",
+            ModDisplayName = "WorldEdit",
+        };
 
 		public void Initialize(TimedTask timer)
 		{
